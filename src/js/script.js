743500112f58a04134572d097ce49a59f6cf415c
Nastya <nasticbazlic@gmail.com>
2021-01-25 23:08:49 -0900

    footer

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,20 @@ const slider = tns({
     slideBy: 'page',
     autoplay: false,
     controls: false,
-    nav: false
+    nav: false,
+     responsive: {
+      320: {
+        gutter: 20,
+        items: 1
+      },
+      700: {
+        gutter: 30,
+        items: 1
+      },
+      900: {
+        items: 2
+      }
+    }
   });
 
   document.querySelector('.prev').addEventListener('click', function () {
